76b02ff2163c45d283a3a766cd9b27817a52dcd9
Val <Val@147.171.164.226>
2012-05-15 16:13:24 +0200

    test

---
+++ Bridge-Chinois/src/bridge/chinois/Poid.java
@@ -0,0 +1,26 @@
+/*
+Auteur : ZIANE-CHERIF Mohammed-El-Amine
+Date de Creation 12/05/2012 : 00:01
+Date de Derni�re modification 12/05/2012 : 18:54
+*/
+
+enum Rang {
+	DEUX(0),TROIS(1),QUATRE(2),CINQ(3),SIX(4),SEPT(5),HUIT(6),
+	NEUF(7),DIX(8),VALET(9),DAME(10),ROI(11),AS(12);
+	
+	// -------------------------------------Attribute-------------------------------------
+	int rang;
+	
+        test
+	// -------------------------------------Constructor-------------------------------------
+	Rang(int i)
+	{
+		rang = i;
+	}
+	
+	// -------------------------------------Accesseur-------------------------------------
+	public int getRang()
+	{
+		return rang;
+	}
+}
